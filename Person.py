8989164a10b503f508671ed9864f6fa14a90d8b7
Victor Kanaykin <Kanaykinvv@mail.ru>
2021-12-07 22:11:38 +0300

    change Person

--- Person.py
+++ Person.py
@@ -2,7 +2,41 @@ class Person:
     name = ""
     cash = 0.0
     experience = 0
+
     strength = 0
+    strength_bonus = 0
+    strength_add = 0
+
     agility = 0
+    agility_bonus = 0
+    agility_add = 0
+
     intellect = 0
+    intellect_bonus = 0
+    intellect_add = 0
+
     initiative = 0
+    initiative_bonus = 0
+    initiative_add = 0
+
+    health_current = 0
+    health_bonus = 0
+    health_add = 0
+    health_effect = 0
+
+    energy_current = 0
+    energy_bonus = 0
+    energy_add = 0
+    energy_effect = 0
+
+    persistence_bonus = 0
+    persistence_add = 0
+    persistence_effect = 0
+
+    reaction_bonus = 0
+    reaction_add = 0
+    reaction_effect = 0
+
+    consciousness_bonus = 0
+    consciousness_add = 0
+    consciousness_effect = 0
