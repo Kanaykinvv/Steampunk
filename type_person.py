49dd06f36a350740ff2451eec15d2d664bd81c03
Victor Kanaykin <Kanaykinvv@mail.ru>
2021-12-17 22:49:45 +0300

    change Person

--- type_person.py
+++ type_person.py
@@ -1,1 +1,14 @@
 # Указаны типы персонажей и их возможные параметры
+
+Warrior = {
+    "strength"      : 0,
+    "agility"       : 0,
+    "intellect"     : 0,
+    "initiative"    : 0,
+    "health"        : 0,
+    "energy"        : 0,
+    "persistence"   : 0,
+    "reaction"      : 0,
+    "consciousness" : 0,
+    "burden"        : 0,
+}
