f89307fad7a5ae8c4af5d3dfc31bac2d699cbf78
Victor Kanaykin <Kanaykinvv@mail.ru>
2021-12-26 23:38:12 +0300

    change items

---
+++ items.py
@@ -0,0 +1,30 @@
+
+
+name
+initiative_effect
+health_effect
+energy_effect
+persistence_effect
+reaction_effect
+consciousness_effect
+burden_effect
+athletics_effect
+
+"athletics_effect"  # Навык - Атлетика
+"no_weapons_effect"  # Навык - Без оружия
+"endurance_effect"  # Навык - Выносливость
+"throwing_effect"  # Навык - Метание
+"heavy_weapon_effect"  # Навык - Тяжёлое оружие
+"cold_weapon_effect"  # Навык - Холодное оружие
+"gambl_effect"  # Навык - Азартные игры
+"acrobatics_effect"  # Навык - Акробатика
+"theft_effect"  # Навык - Воровство
+"light_weapon_effect"  # Навык - Лёгкое оружие
+"traps_effect"  # Навык - Ловушки
+"observation_effect"  # Навык - Наблюдательность
+"hack_effect"  # Навык - Взлом
+"hightech_weapons_effect"  # Навык - Высокотехнологичное оружие
+"engineering_effect"  # Навык - Инженерия
+"medicine_effect"  # Навык - Медицина
+"negotiation_effect"  # Навык - Переговоры
+"trade_effect"  # Навык - Торговля
