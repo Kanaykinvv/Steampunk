dd869e024e95091ab1f98ed9aed6d66a7c0b96a6
Victor Kanaykin <Kanaykinvv@mail.ru>
2021-12-27 23:13:42 +0300

    change items

--- items.py
+++ items.py
@@ -1,30 +1,32 @@
 
-
-name
-initiative_effect
-health_effect
-energy_effect
-persistence_effect
-reaction_effect
-consciousness_effect
-burden_effect
-athletics_effect
-
-"athletics_effect"  # Навык - Атлетика
-"no_weapons_effect"  # Навык - Без оружия
-"endurance_effect"  # Навык - Выносливость
-"throwing_effect"  # Навык - Метание
-"heavy_weapon_effect"  # Навык - Тяжёлое оружие
-"cold_weapon_effect"  # Навык - Холодное оружие
-"gambl_effect"  # Навык - Азартные игры
-"acrobatics_effect"  # Навык - Акробатика
-"theft_effect"  # Навык - Воровство
-"light_weapon_effect"  # Навык - Лёгкое оружие
-"traps_effect"  # Навык - Ловушки
-"observation_effect"  # Навык - Наблюдательность
-"hack_effect"  # Навык - Взлом
-"hightech_weapons_effect"  # Навык - Высокотехнологичное оружие
-"engineering_effect"  # Навык - Инженерия
-"medicine_effect"  # Навык - Медицина
-"negotiation_effect"  # Навык - Переговоры
-"trade_effect"  # Навык - Торговля
+ITEMS = {
+    "name": {
+        "weight"                    : 0,
+        "description"               : 0,
+        "initiative_effect"         : 0,
+        "health_effect"             : 0,
+        "energy_effect"             : 0,
+        "persistence_effect"        : 0,
+        "reaction_effect"           : 0,
+        "consciousness_effect"      : 0,
+        "burden_effect"             : 0,
+        "athletics_effect"          : 0,    # Навык - Атлетика
+        "no_weapons_effect"         : 0,    # Навык - Без оружия
+        "endurance_effect"          : 0,    # Навык - Выносливость
+        "throwing_effect"           : 0,    # Навык - Метание
+        "heavy_weapon_effect"       : 0,    # Навык - Тяжёлое оружие
+        "cold_weapon_effect"        : 0,    # Навык - Холодное оружие
+        "gambl_effect"              : 0,    # Навык - Азартные игры
+        "acrobatics_effect"         : 0,    # Навык - Акробатика
+        "theft_effect"              : 0,    # Навык - Воровство
+        "light_weapon_effect"       : 0,    # Навык - Лёгкое оружие
+        "traps_effect"              : 0,    # Навык - Ловушки
+        "observation_effect"        : 0,    # Навык - Наблюдательность
+        "hack_effect"               : 0,    # Навык - Взлом
+        "hightech_weapons_effect"   : 0,    # Навык - Высокотехнологичное оружие
+        "engineering_effect"        : 0,    # Навык - Инженерия
+        "medicine_effect"           : 0,    # Навык - Медицина
+        "negotiation_effect"        : 0,    # Навык - Переговоры
+        "trade_effect"              : 0,    # Навык - Торговля
+    }
+}
