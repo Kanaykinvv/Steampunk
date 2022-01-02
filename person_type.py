a2a43d77b62926dee97af985c440835f1695f6b1
Victor Kanaykin <Kanaykinvv@mail.ru>
2022-01-02 13:56:10 +0300

    new person_type.py

--- person_type.py
+++ person_type.py
@@ -1,8 +1,7 @@
-import random
-from person import Person
+
 # Указаны типы персонажей и их возможные параметры
 
-type_person = {
+types = {
     "warrior" : {                       # Тип - Воин
         "strength": 30,                 # Сила
         "agility": 10,                  # Ловкость
@@ -34,42 +33,3 @@ type_person = {
         "trade": 0,                     # Навык - Торговля
     }
 }
-
-def create_person(type: str, level: int = 1):
-
-    if type in type_person:
-        person = Person(name="MyPers_1", level=level)
-
-        parameter_priority = {k: v for k, v in sorted(type_person[type].items(),
-                                                      key=lambda item: item[1],
-                                                      reverse=True)}
-        # Указано для теста проверки сортировки
-        print(parameter_priority)
-
-        max_random = 0
-        for value in type_person[type].values():
-            max_random += value
-
-        while person.bounty > 0:
-
-            chance = random.randrange(0, max_random, 1)
-
-            current_chance = 0
-            current_key = ""
-
-            for key, value in type_person[type].items():
-                current_chance += value
-                if chance <= current_chance:
-                    current_key = key
-                    break
-
-            person.parameter_up(name=current_key)
-
-        return person
-    else:
-        return None
-
-
-my_test_person = create_person(type="warrior", level=5)
-print(my_test_person)
-my_test_person.update_log_print()
