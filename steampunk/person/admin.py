fcc0988967f6e30156008fdd033987def7328a50
Victor Kanaykin <Kanaykinvv@mail.ru>
2022-01-13 23:03:30 +0300

    [+] person.urls [~] person.view [~] steampunk.urls [+] person.templates.person_all

--- steampunk/person/admin.py
+++ steampunk/person/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Person
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level')
+    list_display_links = ('name',)
+    search_fields = ('name', 'level')
+
 # Register your models here.
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
--- steampunk/person/views.py
+++ steampunk/person/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Person
 
 # Create your views here.
+def person_all(request):
+    template = loader.get_template('person/person_all.html')
+    persons = Person.objects.order_by('name')
+    contex = {'persons': persons}
+    return HttpResponse(template.render(contex, request))
